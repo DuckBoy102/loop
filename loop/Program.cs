92ffa7e0123e2bd2adb71e6aeb4d620d77daccf7
DuckBoy102 <avieln7@gmail.com>
2022-02-06 16:49:02 +0200

    Add project files.

---
+++ loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int i;
+            i = int.Parse(Console.ReadLine());
+            for (i = 0; i < 3; i++);
+            {
+                Console.WriteLine(i * 5);
+            }
+            if (i >= 3)
+                Console.WriteLine("false");
+            else
+                Console.WriteLine("true");
+        }
+    }
+}
